645f13e332eeec30d599123e22f84ded22e0baff
Kiran Ganji <kiranganji2003@gmail.com>
2025-11-27 23:33:51 +0530

    added vendor entity

---
+++ src/main/java/com/app/estore/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.app.estore.repository;
+
+import com.app.estore.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
