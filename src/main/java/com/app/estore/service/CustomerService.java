e0e1ed0c2c9f3ee721095e4309d81388c45e5aa8
Kiran Ganji <kiranganji2003@gmail.com>
2025-12-24 22:59:49 +0530

    find by product category

--- src/main/java/com/app/estore/service/CustomerService.java
+++ src/main/java/com/app/estore/service/CustomerService.java
@@ -1,8 +1,10 @@
 package com.app.estore.service;
 
+import com.app.estore.common.ProductCategory;
 import com.app.estore.request.RegistrationDto;
 import com.app.estore.response.AllProductsDto;
 import com.app.estore.response.Status;
+import org.jspecify.annotations.Nullable;
 
 import java.util.List;
 
@@ -10,4 +12,5 @@ public interface CustomerService {
     Status registerCustomer(RegistrationDto customerRegistrationDto);
     List<AllProductsDto> findAllProducts();
     List<AllProductsDto> findProductsByCostRange(Integer min, Integer max);
+    List<AllProductsDto> getProductsByCategory(ProductCategory category);
 }
