d6e1eaa5bc62aec007841d1494b1946bf96833ce
Kiran Ganji <kiranganji2003@gmail.com>
2026-01-04 13:53:25 +0530

    modified customer file

--- src/main/java/com/app/estore/entity/Customer.java
+++ src/main/java/com/app/estore/entity/Customer.java
@@ -2,7 +2,6 @@ package com.app.estore.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
